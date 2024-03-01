5e7df0091984cdaf005d19d70e1277c85fddd94e
ttasjwi <ttasjwi920@gmail.com>
2024-03-01 15:22:25 +0900

    Jwt 필터 - 시큐리티 설정 적용

--- src/main/kotlin/com/example/jwt/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/jwt/config/SecurityConfig.kt
@@ -1,23 +1,51 @@
 package com.example.jwt.config
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import com.example.jwt.jwt.JwtAccessDeniedHandler
+import com.example.jwt.jwt.JwtAuthenticationEntryPoint
+import com.example.jwt.jwt.TokenAuthenticationFilter
+import com.example.jwt.jwt.TokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
+
+            csrf { disable() }
+
             authorizeHttpRequests {
                 authorize("/api/hello", permitAll)
+                authorize("/api/authenticate", permitAll)
+                authorize("/api/signup", permitAll)
                 authorize(anyRequest, authenticated)
             }
+
+            headers {
+                frameOptions { sameOrigin }
+            }
+
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
+            }
+
+            exceptionHandling {
+                authenticationEntryPoint = jwtAuthenticationEntryPoint()
+                accessDeniedHandler = jwtAccessDeniedHandler()
+            }
+
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(tokenAuthenticationFilter())
         }
         return http.build()
     }
@@ -26,4 +54,17 @@ class SecurityConfig {
     fun webSecurityCustomizer() = WebSecurityCustomizer { web ->
         web.ignoring().requestMatchers("/h2-console/**", "/favicon.ico")
     }
+
+    @Bean
+    fun tokenAuthenticationFilter() = TokenAuthenticationFilter(tokenProvider)
+
+
+    @Bean
+    fun jwtAuthenticationEntryPoint() = JwtAuthenticationEntryPoint()
+
+    @Bean
+    fun jwtAccessDeniedHandler() = JwtAccessDeniedHandler()
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
 }
